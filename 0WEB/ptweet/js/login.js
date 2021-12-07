f692d02be7e60a40fbcff0fc78436939f825667a
choo-exe <21CG0053@ITSNCG.EDU.MX>
2021-12-07 08:51:47 -0700

    13 Objetos Arrys y Local Storage
    Listo las sesiones

--- 0WEB/ptweet/js/login.js
+++ 0WEB/ptweet/js/login.js
@@ -6,9 +6,29 @@ window.onload = function(){
     boton.addEventListener('click',function(){
         let usuario = txtUsuario.value;
         let pass = txtPassword.value;
-        if(usuario.trim() == "admin" && pass.trim() == "123"){
-            alert("Listo");
-        
+        let arregloUsuarios=[
+            {id:1, nombre:"Federica", imagen: "fede.jpg", username:"fedep", password:"123"},
+            {id:1, nombre:"Don Camerino", imagen: "dc.jpg", username:"camerino", password:"123"},
+            {id:1, nombre:"Vivi", imagen: "vivi.jpg", username:"vivi", password:"123"},
+        ];
+        /*console.log(arregloUsuarios[1].nombre);*/
+       /* arregloUsuarios.forEach(function(fila){ /*objeto*/
+          /*  console.log(fila); A LA ANTIGUA*/
+          let encontro = false;
+
+          arregloUsuarios.forEach(fila=>{
+            if (fila.username == usuario.trim() && fila.password == pass.trim()){
+                encontro= true;
+                localStorage.setItem("usuario",  JSON.stringify(fila));
+                let fecha = new Date();
+                localStorage.setItem("fecha", fecha.getFullYear()+"/"+fecha.getMonth()+ "/"+ fecha.getDay()+ " "+ fecha.getHours()+ ":"+ fecha.getMinutes()+ ":"+ fecha.getSeconds());
+            }
+            });
+          
+        /*if(usuario.trim() == "admin" && pass.trim() == "123"){*/
+            if(encontro == true){
+            //redireccionar
+            location.href="./index.html";
         }else{
             document.getElementById("error").innerHTML="Credenciales incorrectas";
             txtUsuario.style.backgroundColor= "#FF0000";
